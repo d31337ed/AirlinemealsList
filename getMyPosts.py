54a586a1aec1438b5c95dc5e0fe2da61ebc99a2a
Anton <anton@de1337ed.ru>
2022-07-20 22:10:16 +0300

    Debug logging removal

--- getMyPosts.py
+++ getMyPosts.py
@@ -16,12 +16,9 @@ class MyPosts:
             current_shift = batch_number*batch_size
             batch_posts_request = method_way+'?domain='+public+'&access_token='+token+'&v=5.131&count='+str(batch_size)+'&offset='+str(current_shift)
             response = requests.get(batch_posts_request).json()['response']['items']
-            print(author_id)
             for item in response:
                 if 'signer_id' in item.keys() and item['signer_id'] == author_id:
-                    print(item['text'])
                     last_strings = item['text'].split('\n')[-3:]
-                    print(last_strings)
                     with open('raw_posts.txt', 'a') as result_file:
                         result_file.write(' '.join(last_strings)+' #Антон_и_БортовоеПитание'+
                                           ' https://vk.com/airlinemeals?w=wall-141742284_'+str(item['id'])+'\n')
